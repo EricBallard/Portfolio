ef980dcf2ca291e60c0334581490b3f72adb315d
Eric Ballard <ericballard7@gmail.com>
2021-12-09 00:54:24 -0500

    ~ LoadingAnim

--- src/components/LoadAnim.js
+++ src/components/LoadAnim.js
@@ -6,26 +6,27 @@ const desiredFPS = 60,
   renderInterval = 1000 / desiredFPS
 
 let playAnimation = true,
-  lastRender = undefined
-
-let cWidth = undefined,
-  cHeight = undefined
-
-function Indicator(x, y) {
-  this.x = x
-  this.y = y
-  this.angle = 0
-  this.angle2 = 0
-  this.velocity = 0
-}
-
-
-let angle = 0
+  lastRender = undefined,
+  animSpeed = 0.075
 
 let indicators = []
 
-var step = 0
+function Indicator(x, y, step, delay, invert, backwards) {
+  this.freeze = false
+  this.risen = false
+  this.x = x
+  this.y = y
+  this.step = step
+  this.delay = delay
+  this.invert = invert
+  this.backwards = backwards
+}
 
+// Canvas dimensions and offsets for centered circular motion
+let cWidth = undefined,
+  cHeight = undefined,
+  xOffset = undefined,
+  yOffset = undefined
 
 const draw = ctx => {
   // Clear canvas
@@ -35,39 +36,73 @@ const draw = ctx => {
   ctx.fillStyle = 'white'
 
   // Draw indicators
-  indicators.forEach(i => {
+  for (let index = 0; index < 5; index++) {
+    const i = indicators[index]
+
     // Save context
     ctx.save()
 
-    ctx.beginPath()
-    // X, y, radius, start angle, end angle
-    ctx.arc(cWidth / 2, cHeight / 2, radius, 0, 2 * Math.PI)
-    ctx.fill()
-    ctx.closePath()
-
-
     // Draw
     ctx.beginPath()
+
     // X, y, radius, start angle, end angle
-    ctx.arc(i.x, i.y, radius, 0, 2 * Math.PI)
+    ctx.arc(i.x, i.y, 15, 0, 2 * Math.PI)
     ctx.fill()
     ctx.closePath()
 
     // Restore context
     ctx.restore()
-      // increase the angle of rotation
-      i.x = (cWidth / 2) + 50 * Math.cos(0.075 * step)
-      i.y = (cHeight / 2) + 50 * Math.sin(0.075 * step)
-      ++step;
 
-  })
+    if (i.freeze) continue
 
-  console.log('rendered indicators!')
+    // Dots must first "rise" up before circular motion
+    if (!i.risen) {
+      // Delay
+      if (i.delay > 0) {
+        i.delay--
+        console.log(index + ' | delay: ' + i.delay)
+        continue
+      }
+
+      // De/Ascend
+      let desiredY = cHeight / 2 + 100
+
+      if (i.invert) {
+        i.y += 2
+
+        if (i.y >= desiredY) i.risen = true
+      } else {
+        i.y -= 2
+
+        if (i.y <= cHeight - desiredY) i.risen = true
+      }
+    } else {
+      // Ciruclar motion
+      i.x = xOffset + 100 * Math.cos(animSpeed * i.step)
+      i.y = yOffset + 100 * Math.sin(animSpeed * i.step)
+
+      if (i.backwards) i.step--
+      else i.step++
+
+      // 1 full lap
+      if (index === 2 && i.step === 43) {
+        indicators[1].freeze = true
+        i.freeze = true
+      }
+
+      console.log(index + ' Step ' + i.step)
+
+      if (index === 4 && i.step === -1) {
+        indicators[3].freeze = true
+        i.freeze = true
+      }
+    }
+  }
 }
 
 const animate = ctx => {
   // Handles drawing graphic in a timely manner
-  const now =  Date.now()
+  const now = Date.now()
 
   if (!lastRender || now - lastRender >= renderInterval) {
     lastRender = now
@@ -77,9 +112,6 @@ const animate = ctx => {
   // Animation loop
   requestAnimationFrame(() => animate(ctx))
 }
-
-// Constants
-const radius = 15
 
 // Component
 function LoadAnim() {
@@ -92,14 +124,22 @@ function LoadAnim() {
     if (canvas.current) {
       // Get graphics context
       ctx.current = canvas.current.getContext('2d')
+
       cWidth = canvas.current.width
       cHeight = canvas.current.height
 
-      // Init indicators
-      for (let i = 0; i < 1; i++) {
-        indicators[i] = new Indicator(cWidth / 2, cHeight / 2)
-        indicators[i] = new Indicator(cWidth / 2, cHeight / 2)
-      }
+      xOffset = cWidth / 2
+      yOffset = cHeight / 2
+
+      // Init indicators - X, Y, STEP, DELAY, INVERT, BACKTRACK
+      indicators[0] = new Indicator(xOffset, yOffset, 0, false, false) // Center
+      indicators[0].freeze = true
+
+      indicators[1] = new Indicator(xOffset, yOffset, 63, 0, false, false)
+      indicators[2] = new Indicator(xOffset, yOffset, 21, 0, true, false)
+
+      indicators[3] = new Indicator(xOffset, yOffset, 63, 21, false, true)
+      indicators[4] = new Indicator(xOffset, yOffset, 21, 21, true, true)
 
       // Start animation
       animate(ctx.current)
